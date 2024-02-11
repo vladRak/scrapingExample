4372d730f305f57fce64bab7fb0dce0371b91f15
@vrakytianskyi <vladyslav.rakytianskyi@vetasi.com>
2024-02-11 10:09:14 +0200

    TEST Commit

---
+++ src/main/java/scrapingExample/scrapingExample/App.java
@@ -0,0 +1,13 @@
+package scrapingExample.scrapingExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
